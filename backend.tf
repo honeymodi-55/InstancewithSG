aeb50d2d0094db178787bcc7e6cccbc0c4334226
Honey Modi <Honey>
2021-06-07 15:12:00 -0400

    Creating an Independent Instance

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
       bucket = "honey-aws-terraform"
-      key = "honeyterraform"
+      key = "s3://honey-aws-terraform/honeyterraform"
       region = "ca-central-1"
     }
 }
