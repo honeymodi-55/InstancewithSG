82b11652f52e8c65513098d337dd7bd423472471
vMahidhar <53318668+vMahidhar@users.noreply.github.com>
2021-06-08 17:48:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "First_Instance" {
   ami = "ami-0277fbe7afa8a33a6"
   instance_type = "t2.micro"
   key_name = "trail-key"
-  security_groups = [ "${aws_security_group.SG_Honey.name}" ]
   tags = {
       Name = "terraform-honey"
       }
@@ -37,25 +36,25 @@ resource "aws_security_group" "SG_Honey" {
   }  
 }
 
-#resource for creating a Storage S3 Bucket
-# resource "aws_s3_bucket" "Bucket1" {
-#   bucket = "honeys-terraform-bucket"
-#   acl = "private"
+resource for creating a Storage S3 Bucket
+resource "aws_s3_bucket" "Bucket1" {
+  bucket = "honeys-terraform-bucket"
+  acl = "private"
 
-#   tags = {
-#     Name = "terraform-honey"
-#   }
+  tags = {
+    Name = "terraform-honey"
+  }
 
-#   versioning {
-#     enabled = true
-#   }
-# }
+  versioning {
+    enabled = true
+  }
+}
 
 #creating a connection between the security group and instance
-# resource "aws_network_interface_sg_attachment" "ConnectiontoSG" {
-#   security_group_id = aws_security_group.SG_Honey.id
-#   network_interface_id = aws_instance.First_Instance.primary_network_interface_id
-# }
+resource "aws_network_interface_sg_attachment" "ConnectiontoSG" {
+  security_group_id = aws_security_group.SG_Honey.id
+  network_interface_id = aws_instance.First_Instance.primary_network_interface_id
+}
 
 
   provider "aws" {
